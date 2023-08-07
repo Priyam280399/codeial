9ac233b3d019a8b045acbbd5615f3582b80a2695
Priyam Kumar Maurya <priyammaurya28032000@gmail.com>
2023-08-07 22:08:36 +0530

    commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,9 +2,9 @@
 //require the library
 const mongoose = require('mongoose');
 
-mongoose.set('strictQuery', true);
+// mongoose.set('strictQuery', true);
 //connect to the database
-mongoose.connect('mongodb+srv://gudun28032000:gudun28032000@cluster0.c9p8qjs.mongodb.net/');
+mongoose.connect('mongodb+srv://gudun28032000:gudun28032000@cluster0.c9p8qjs.mongodb.net/Codeial');
 
 //acquire the connection(to check if it's successful)
 const db = mongoose.connection;
